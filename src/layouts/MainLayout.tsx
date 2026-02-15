9626bbd8df54f98e09c2bb2cc93802c7f164abb1
jshmslf <jshmslf@gmail.com>
2026-02-15 14:10:44 +0800

    feat: fixed animation

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,19 @@
 import Profile from "../components/Profile";
 import "../App.scss";
 import { Outlet } from "react-router-dom";
+import { motion } from "motion/react"
 
 function MainLayout() {
     return (
-    <>    
-        <Profile />
+    <> 
+        <motion.div
+            initial={{ opacity: 0}}
+            animate={{opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.1, ease: "easeOut" }}
+        >
+            <Profile />
+        </motion.div>
+            
         <main className="main-layout">
             <Outlet/>
         </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,34 @@ import Project from "../components/Project";
 import SocialLinks from "../components/SocialLinks";
 import Tech from "../components/Tech";
 import '../styles/Home.scss';
+import { motion } from "motion/react"
 
 function Home() {
   return (
     <>
-      <section className="left">
-        <About />
-        <Tech />
-        <Project />
-      </section>
+      <motion.div
+        initial={{y: 25, opacity: 0}}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.125, ease: "easeOut" }}
+      >
+        <section className="left">
+          <About />
+          <Tech />
+          <Project />
+        </section>
+      </motion.div>
 
+      <motion.div
+        initial={{y: 35, opacity: 0}}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2, ease: "easeOut" }}
+      >
       <aside className="right">
         <Experience />
         <SocialLinks/>
         <Likes />
-      </aside>
+        </aside>
+      </motion.div>
     </>
   );
 }
